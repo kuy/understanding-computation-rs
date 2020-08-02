e6db81d58f06151041b867039070fd08b747a3c4
Yuki Kodama <endflow.net@gmail.com>
2020-08-03 02:00:34 +0900

    Start line

---
+++ README.md
@@ -0,0 +1,3 @@
+## Understanding Computation in Rust
+
+Reading [this book](https://computationbook.com/) and writing code in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let exp = Add {
+        left: Multiply {
+            left: Number(1),
+            right: Number(2),
+        },
+        right: Multiply {
+            left: Number(3),
+            right: Number(4),
+        },
+    };
+    println!("{:?}", exp);
+}
+
+#[derive(Debug)]
+struct Number(i32);
+
+#[derive(Debug)]
+struct Add {
+    pub left: Multiply,
+    pub right: Multiply,
+}
+#[derive(Debug)]
+struct Multiply {
+    pub left: Number,
+    pub right: Number,
 }
